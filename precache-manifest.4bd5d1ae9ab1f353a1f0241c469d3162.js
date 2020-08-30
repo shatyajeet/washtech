6f85677f680857aa2f9de14862467b5af620f316
Shatyajeet Parida <shatyajeet@gmail.com>
2020-08-30 12:05:42 +0530

    Updates

---
+++ precache-manifest.4bd5d1ae9ab1f353a1f0241c469d3162.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d23246a7b7b8e2fbfafb1356a4471f99",
+    "url": "/washtech/index.html"
+  },
+  {
+    "revision": "bab5e2d3390f5f15a889",
+    "url": "/washtech/static/css/main.0e05233f.chunk.css"
+  },
+  {
+    "revision": "7d7bf43d23ac58c8c94f",
+    "url": "/washtech/static/js/2.4bcb6701.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/washtech/static/js/2.4bcb6701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bab5e2d3390f5f15a889",
+    "url": "/washtech/static/js/main.7be42a6f.chunk.js"
+  },
+  {
+    "revision": "fe5a691ba9f2a08ef995",
+    "url": "/washtech/static/js/runtime-main.14e88206.js"
+  },
+  {
+    "revision": "ef1af8da0c19e7fcbe9b4ce6fbeeb7b4",
+    "url": "/washtech/static/media/logo.ef1af8da.svg"
+  },
+  {
+    "revision": "ce85c3f1a551bff7ce16901c7a684c08",
+    "url": "/washtech/static/media/phone.ce85c3f1.svg"
+  }
+]);
